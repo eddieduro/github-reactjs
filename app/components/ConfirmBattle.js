13bbf234e678ee55a8966f05ada95a090dfbe24e
eddie duro <e.jamesduro@gmail.com>
2016-06-24 00:46:37 -0700

    user detail component renders props from github api

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,8 +1,9 @@
 var React = require('react');
-var styles = require('../styles').transparentBg;
+var styles = require('../styles');
 var PropTypes = React.PropTypes;
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
+var UserDetailWrapper = require('./UserDetailWrapper');
 
 function puke (object) {
 	return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -13,12 +14,12 @@ function ConfirmBattle (props) {
 		? <p> LOADING! </p>
 		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
-        <div className='col-sm-8 col-sm-offset-2'>
+        <UserDetailWrapper header='Player One'>
           <UserDetails info={props.playersInfo[0]} />
-        </div>
-        <div className='col-sm-8 col-sm-offset-2'>
-        	<UserDetails info={props.playersInfo[1]} />
-        </div>
+        </UserDetailWrapper>
+        <UserDetailWrapper header='Player Two'>
+          <UserDetails info={props.playersInfo[1]} />
+        </UserDetailWrapper>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
             <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
