27a708061950d38aa51525c43f9e4e639be33d1f
eddie duro <e.jamesduro@gmail.com>
2016-06-23 20:00:46 -0700

    update routes for main and home components

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+	render: function () {
+		return (
+			<div> Welcome from Home!</div>
+		)
+	}
+})
+
+module.exports = Home;
--- app/index.js
+++ app/index.js
@@ -1,73 +1,8 @@
-var USER_DATA = {
-	name: 'Eddie Duro',
-	username: 'eddieduro',
-	image: 'https://avatars1.githubusercontent.com/u/7972722?v=3&s=460'
-}
-
-var React = require('react')
-var ReactDOM = require('react-dom')
- 
-/*
-	Focused
-	Independent
-	Reusable
-	Small 
-	Testable
-*/
-
-var ProfilePic = React.createClass({
-	render: function () {
-		return <img src={this.props.imageUrl} style={{height: 100, width:100}} />
-	}
-});
-
-var Link = React.createClass({
-	changeURL: function () {
-		window.location.replace(this.props.href)
-	},
-	render: function () {
-		return(
-			<span style={{color: 'blue', cursor: 'pointer'}}
-			onClick={this.changeURL} >
-			{this.props.children}
-			</span>
-		)
-	}
-})
-
-var ProfileLink = React.createClass({
-	render: function () {
-		return(
-				<Link href={'https://www.github.com/' + this.props.username}>
-				{this.props.username}
-				</Link>
-		)
-	}
-});
-
-var ProfileName = React.createClass({
-	render: function () {
-		return (
-			<div>
-			{this.props.name}
-			</div>
-		)
-	}
-});
-
-var Avatar = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image} />
-				<ProfileName name={this.props.user.name} />
-				<ProfileLink username={this.props.user.username} />
-			</div>
-		)
-	}
-});
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
 ReactDOM.render(
-	<Avatar user={USER_DATA} />,
+	routes,
 	document.getElementById('app')
 );
