4a0661640c51a733fbb5e3b114206bc20088be5b
eddie duro <e.jamesduro@gmail.com>
2016-06-23 20:09:18 -0700

    create first prompt

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,28 @@
+var React = require('react');
+
+var PromptContainer = React.createClass({
+	render: function () {
+		<div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+			<h1> Some Header text </h1>
+			<div className="col-sm-12">
+				<form>
+					<div className="form-group">
+						<input
+							className="form-control"
+							placeholder="Github Username"
+							type="test" />
+					</div>
+					<div className="form-group col-sm-4 col-sm-offset-4">
+						<button 
+							className="btn btn-block btn-success"
+							type="submit">
+							Continue
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	}
+});
+
+module.exports = PromptContainer;
